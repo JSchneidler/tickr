6f418da23ac3dae1a2f8a13d4aff14237176d8a4
Jordan Schneidler <jordan.schneidler@gmail.com>
2026-02-15 13:27:18 -0600

    Fix build

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "./generated/prisma/client";
-import { PrismaLibSql } from '@prisma/adapter-libsql';
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is required");
 
 const adapter = new PrismaLibSql({
